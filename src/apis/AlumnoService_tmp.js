07cf741f46a5080d645bbe5033aa5de958934e58
MariV23 <124789760+MariV23@users.noreply.github.com>
2025-11-25 20:04:25 -0600

    Aceptar cambios del merge

--- src/apis/AlumnoService_tmp.js
+++ src/apis/AlumnoService_tmp.js
@@ -2,7 +2,10 @@ import axios from "axios";
 
 const API_URL = "http://localhost:8080/api/alumnos";
 
-export const getAlumnoTalleres = async (idusuario) =>{
-    const res = await axios.get(`${API_URL}/${idusuario}/talleres`)
-    return res.data;
+export const crearAlumnosDesdePostulaciones = async (postulaciones) => {
+  const response = await axios.post(
+    `${API_URL}/crear-desde-postulaciones`,
+    postulaciones
+  );
+  return response.data;
 };
--- src/apis/ArchivoService_tmp.js
+++ src/apis/ArchivoService_tmp.js
@@ -2,15 +2,26 @@ import axios from "axios";
 
 const API_URL = "http://localhost:8080/api/archivos";
 
-//Archivo de actividad
-export const uploadArchivo = async (formData) =>{
-    const res = await axios.post(`${API_URL}/upload`,formData, {
-        headers: { "Content-Type": "multipart/form-data" }
-    });
-    return res.data;
+export const uploadArchivoPostulacion = async (formData) => {
+  const res = await axios.post(
+    "http://localhost:8080/api/archivos/upload",
+    formData,
+    {
+      headers: { "Content-Type": "multipart/form-data" }
+    }
+  );
+
+  return res.data;
 };
 
-export const getArchivosActividad = async(idActividad) =>{
-    const res = await axios.get(`${API_URL}/actividad/${idActividad}`);
-    return res.data;
+export const getEvidenciasByActividad = async (idActividad) => {
+  try {
+    const response = await axios.get(
+      `${API_URL}/${idActividad}/evidencias`
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error al obtener evidencias:", error);
+    throw error;
+  }
 };
